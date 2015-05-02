7290aa3a47d8c7f345e4da7e6050a546402ae0b6
Arpit Jain <arpitjain099@gmail.com>
2015-05-02 07:27:34 +0530

    almost done

--- processtasksheet.py
+++ processtasksheet.py
@@ -4,23 +4,47 @@ import codecs
 client = MongoClient()
 db=client.quizdb
 from time import gmtime, strftime
-collection=db.tasks
 #print db
-username="tvpsir"
-coursename="cs315_1"
+#username="tvpsir"
+#coursename="cs315_1"
 collection=db.questionbank
 #collection_rec.insert({"ada":"dada"})
 #collection_rec.find_one({u'username':username})["count"]=11
 #print collection_rec
 #count= collection_rec.find_one({"username":username})['count']
-
+import shutil, errno
 #print count
 #print val
 #defaultquestions=["Is this image profane or not?","Is this comment profane or not?", "Categorize this image into 5 categories","Categorize this book","Give 5 tags to this article","Give 5 tags for this image"," Which of the variants look good to you?","Which of the variants matches the description?","Is this ad appealing to you?","Grade the sentiment expressed in this tweet","Grade the sentiment expressed in this user comment","From the product review, is the customer satisfied?", "Which image is better?", "Which design is more soothing?","How will you rank this MP3 file?","Help us rate this place"]
-fi=codecs.open("data.js","w",encoding="utf-8")
+def copyDirectory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    # Directories are the same
+    except shutil.Error as e:
+        print('Directory not copied. Error: %s' % e)
+    # Any error saying that the directory doesn't exist
+    except OSError as e:
+        print('Directory not copied. Error: %s' % e)
+
+import sys
+import os
+coursename=sys.argv[1]
+coursename=coursename.replace("uploads/","")
+coursename=coursename.replace(".csv","")
+os.chdir("projects/")
+#os.system("pwd")
+os.system("cordova create "+coursename+" com.example.hello "+coursename)
+
+root_src_dir = '../www/'
+root_dst_dir = "../projects/"+coursename+"/www/"
+shutil.rmtree("../projects/"+coursename+"/www/")
+copyDirectory("../www/","../projects/"+coursename+"/www/")
+
+
+fi=codecs.open(coursename+"/www/data.js","w",encoding="utf-8")
 fi.write("var sampletask=[")
 count=1
-with open('inputdata.csv', 'rb') as csvfile:
+with open("../"+sys.argv[1], 'rb') as csvfile:
 	spamreader = csv.reader(csvfile)
 	#spamreader=spamreader[17:]
 	for index,row in enumerate(spamreader):
@@ -44,3 +68,6 @@ with open('inputdata.csv', 'rb') as csvfile:
 			
 fi.write("]")
 fi.close()
+os.chdir("../projects/"+coursename)
+os.system("cordova platform add android")
+os.system("cordova build android")
